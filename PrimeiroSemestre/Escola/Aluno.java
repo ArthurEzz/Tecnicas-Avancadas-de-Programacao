ebcaffc32ef9980e31b05d8205c486d4a43eb2b2
ArthurEzz <arthurpinheiro218@gmail.com>
2026-03-10 15:48:31 -0300

    Update Aluno.java

--- PrimeiroSemestre/Escola/Aluno.java
+++ PrimeiroSemestre/Escola/Aluno.java
@@ -1,1 +1,24 @@
 
+package Escola;
+
+public class Aluno {
+    private String alunos;
+    private String ra;
+    
+    public void setAlunos(String a) {
+    alunos = a;
+    }
+    
+    public String getAlunos(){
+    return alunos;
+    }
+    
+    public void setRa(String r){
+    ra = r;
+    }
+    
+    public String getRa(){
+    return ra;
+    }
+
+}
